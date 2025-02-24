59e42f461d3e8d58e49539c06bf76c8963b42f2d
WilliamBayona <w.bayona@uniandes.edu.co>
2025-02-24 09:14:12 -0500

    Predictor (Sin Regresion) Listo ✅

--- Aplicacion/paginas/predictor.py
+++ Aplicacion/paginas/predictor.py
@@ -1,18 +1,19 @@
-from dash import dcc, html
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
 
+# Lista de estados y fuentes de anuncios
+states = ['DC', 'IN', 'VA', 'WA', 'NY', 'CA', 'AZ', 'NC', 'TX', 'GA', 'FL', 'AL', 'MD', 'CO', 'NM', 'IL', 'TN', 'AK', 'MA', 'NJ', 'OR', 'DE', 'PA', 'IA', 'SC', 'MN', 'MI', 'KY', 'WI', 'OH', 'CT', 'RI', 'NV', 'UT', 'MO', 'OK', 'NH', 'NE', 'LA', 'ND', 'AR', 'KS', 'ID', 'HI', 'MT', 'VT', 'SD', 'WV', 'MS', 'ME', 'WY']
+sources = ['RentLingo', 'Listanza', 'ListedBuy', 'RentDigs.com', 'GoSection8', 'RealRentals', 'RENTOCULAR', 'rentbits', 'Home Rentals', 'Real Estate Agent', 'RENTCafé', 'tenantcloud']
+
+# Layout con formulario
 layout = html.Div([
-    # Título
-    html.H1("Predictor de Precios", style={'textAlign': 'center', 'color': '#5A3E91'}),
-    
-    # Contenedor principal
+    html.H1("Predictor de Precios",  style={'font-weight': 'bold', 'textAlign': 'center'}),
     html.Div(style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'}, children=[
-        
-        # Imagen del apartamento
         html.Div([
             html.Img(src="/assets/apartamento2.png", style={'width': '100%', 'maxWidth': '500px', 'borderRadius': '10px'})
         ], style={'flex': '1', 'padding': '20px'}),
-        
-        # Formulario de entrada
         html.Div(style={'flex': '1', 'padding': '20px'}, children=[
             
             html.Label("Número de Baños"),
@@ -21,20 +22,46 @@ layout = html.Div([
             html.Label("Número de Habitaciones"),
             dcc.Input(id="bedrooms-input", type="number", min=0, placeholder="Ej. 3", style={'width': '100%'}),
             
-            html.Label("Tiene foto (sí/no)"),
-            dcc.Input(id="has-photo-input", type="text", placeholder="Ej. sí", style={'width': '100%'}),
+            html.Label("Tiene foto"),
+            dcc.Dropdown(
+                id='has-photo-input',
+                options=[
+                    {'label': 'Sí', 'value': 'Sí'},
+                    {'label': 'No', 'value': 'No'}
+                ],
+                placeholder="Selecciona una opción",
+                style={'width': '100%'}
+            ),
             
-            html.Label("Se permiten mascotas(sí/no)"),
-            dcc.Input(id="pets-allowed-input", type="text", placeholder="Ej. sí/no", style={'width': '100%'}),
+            html.Label("Se permiten mascotas"),
+            dcc.Dropdown(
+                id='pets-allowed-input',
+                options=[
+                    {'label': 'Sí', 'value': 'Sí'},
+                    {'label': 'No', 'value': 'No'}
+                ],
+                placeholder="Selecciona una opción",
+                style={'width': '100%'}
+            ),
             
             html.Label("Tamaño (pies cuadrados)"),
             dcc.Input(id="square-feet-input", type="number", min=0, placeholder="Ej. 1200", style={'width': '100%'}),
             
             html.Label("Estado en que se ubica"),
-            dcc.Input(id="state-input", type="text", placeholder="Ej. NY", style={'width': '100%'}),
+            dcc.Dropdown(
+                id='state-input',
+                options=[{'label': state, 'value': state} for state in states],
+                placeholder="Selecciona un estado",
+                style={'width': '100%'}
+            ),
             
             html.Label("Origen del anuncio"),
-            dcc.Input(id="source-input", type="text", placeholder="Ej. Craigslist", style={'width': '100%'}),
+            dcc.Dropdown(
+                id='source-input',
+                options=[{'label': source, 'value': source} for source in sources],
+                placeholder="Selecciona una fuente",
+                style={'width': '100%'}
+            ),
             
             html.Button("Calcular Precio", id="predict-button", style={
                 'width': '100%', 'backgroundColor': '#5A3E91', 'color': 'white', 'padding': '10px',
@@ -49,3 +76,4 @@ layout = html.Div([
         style={'textAlign': 'center', 'color': '#5A3E91', 'fontWeight': 'bold', 'marginTop': '20px'}
     )
 ])
+
